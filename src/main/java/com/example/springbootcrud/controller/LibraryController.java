7efb6e5015e37bb74b572778f83ac36689f9fd97
noparamin <noparamin@naver.com>
2022-02-27 23:45:01 +0900

    add controller

---
+++ src/main/java/com/example/springbootcrud/controller/LibraryController.java
@@ -0,0 +1,24 @@
+package com.example.springbootcrud.controller;
+
+import com.example.springbootcrud.service.LibraryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/library")
+@RequiredArgsConstructor
+public class LibraryController {
+    private final LibraryService libraryService;
+
+    @GetMapping("/book")
+    public ResponseEntity readBooks(@RequestParam(required = false) String isbn) {
+        if(isbn == null) {
+            return ResponseEntity.ok(libraryService.readBooks());
+        }
+        return ResponseEntity.ok(libraryService.readBook(isbn));
+    }
+}
