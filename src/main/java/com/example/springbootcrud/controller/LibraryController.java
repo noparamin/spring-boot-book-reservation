45a9fc08cd1819bf2855855bf4041b044be07bb6
noparamin <noparamin@naver.com>
2022-02-28 23:30:02 +0900

    update controller

--- src/main/java/com/example/springbootcrud/controller/LibraryController.java
+++ src/main/java/com/example/springbootcrud/controller/LibraryController.java
@@ -1,12 +1,11 @@
 package com.example.springbootcrud.controller;
 
+import com.example.springbootcrud.model.Book;
+import com.example.springbootcrud.model.request.BookCreationRequest;
 import com.example.springbootcrud.service.LibraryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/library")
@@ -20,5 +19,15 @@ public class LibraryController {
             return ResponseEntity.ok(libraryService.readBooks());
         }
         return ResponseEntity.ok(libraryService.readBook(isbn));
+    }
+
+    @GetMapping("/book/{bookId}")
+    public ResponseEntity<Book> readBook (@PathVariable Long bookId){
+        return ResponseEntity.ok(libraryService.readBook(bookId));
+    }
+
+    @PostMapping("/book")
+    public ResponseEntity<Book> createBook (@RequestBody BookCreationRequest request) {
+        return ResponseEntity.ok(libraryService.createBook(request));
     }
 }
